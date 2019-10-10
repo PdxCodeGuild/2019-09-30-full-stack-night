d9233af56301f94b345c3eb2c594ad81190f0f80
justin <jhiester@gmail.com>
2019-10-09 18:47:59 -0700

    refactored unit converter

--- 1 Python/solutions/unit_converter.py
+++ 1 Python/solutions/unit_converter.py
@@ -1,29 +1,27 @@
-# dictionary mapping units to conversion factors for meters
-conversion_factors = {
-    'ft': 0.3048,
-    'mi': 1609.34,
-    'm': 1,
-    'km': 1000,
-    'yd': 0.9144,
-    'in': 0.0254
-}
+def convert_units(data):
+    conversion_factors = {
+        'ft': 0.3048,
+        'mi': 1609.34,
+        'm': 1,
+        'km': 1000,
+        'yd': 0.9144,
+        'in': 0.0254
+    }
+    value, unit_from, unit_to = data
+    converted_m = conversion_factors[unit_from] * value
+    return round(converted_m / conversion_factors[unit_to], 2)
 
-user_input = input('\nenter the distance: ')
-convert_to_unit = input('\nenter units to convert to: ')
 
-# split the user input on the space to separate the value from the unit
-user_input_split = user_input.split(" ")
+def main():
 
-value = int(user_input_split[0])
-unit = user_input_split[1]
+    user_input = input('\nenter the distance: ')
+    convert_to_unit = input('\nenter units to convert to: ')
 
-# convert to meters using the unit the user entered as the key for the conversion_factor dictionary
-converted_m = conversion_factors[unit] * value
+    user_input_split = user_input.split(" ")
 
-# convert to the specified unit using the other unit the user entered as the key for the conversion_factor dictionary
-converted_other = round(converted_m / conversion_factors[convert_to_unit], 2)
+    value = int(user_input_split[0])
+    unit = user_input_split[1]
 
-# print statement for quick verification. left in, but unnecessary for final code
-# print(f'\n{value} {unit} is {converted_m} m.')
+    print(f'\n{value} {unit} is {convert_units((value, unit, convert_to_unit))} {convert_to_unit}.\n')
 
-print(f'\n{value} {unit} is {converted_other} {convert_to_unit}.\n')
+main()
