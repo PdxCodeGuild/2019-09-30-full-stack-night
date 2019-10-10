1ff49bc5f818a1047bd1fe1542b65ab69d0d9033
justin <jhiester@gmail.com>
2019-10-09 18:21:40 -0700

    small changes

--- 1 Python/solutions/rot13Cipher.py
+++ 1 Python/solutions/rot13Cipher.py
@@ -10,26 +10,6 @@ def cipher_string_rotation(a_string, rotation_amount = 13):
         encoded_string += string.ascii_lowercase[(index + rotation_amount) % 26]
     return encoded_string
 
-def cipher_string_rotation(a_string, rotation_amount = 13):
-    encoded_string = ''
-    for char in a_string:
-        index = string.ascii_lowercase.find(char)
-        encoded_string += string.ascii_lowercase[(index + rotation_amount) % 26]
-    return encoded_string
-
-def rotn(text, n):
-    alphabet = 'abcdefghijklmnopqrstuvwxyz'
-    output = ''
-    for char in text:
-        index = alphabet.find(char)
-        index += n
-
-        while index >= len(alphabet):
-            index -= len(alphabet)
-        output += alphabet[index]
-    return output
-
-
 def main():    
     while True:
         rotation = input('enter rotation (enter a \'q\' to quit): ')
