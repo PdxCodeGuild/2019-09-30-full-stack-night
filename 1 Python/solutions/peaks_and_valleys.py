33fdab70505ca459fb3b804b3231cd7503de48d5
justin <jhiester@gmail.com>
2019-10-18 20:48:15 -0700

    changed file name and added original back into repo

--- 1 Python/solutions/peaks_and_valleys.py
+++ 1 Python/solutions/peaks_and_valleys.py
@@ -1,16 +1,46 @@
-def peaks(data, indices):
-    return [i for i in [i for i in indices if (i - 1) in indices and (i + 1) in indices] if data[i] >= data[i - 1] and data[i] >= data[i + 1]]
 
-def valleys(data,indices):
-    return [i for i in [i for i in indices if (i - 1) in indices and (i + 1) in indices] if data[i] <= data[i - 1] and data[i] <= data[i + 1]]
+def peaks(data):
+    peaks = []
+    indices = list(range(len(data)))
+
+    for i in indices:
+        if (i - 1) in indices and (i + 1) in indices:
+            if data[i] >= data[i - 1] and data[i] >= data[i + 1]:
+                # print(f'{i} is a peak')
+                peaks.append(i)
+            continue
+    return peaks
+
+
+def valleys(data):
+    valleys = []
+    indices = list(range(len(data)))
+
+    for i in indices:
+        if (i - 1) in indices and (i + 1) in indices:
+            if data[i] <= data[i - 1] and data[i] <= data[i + 1]:
+                # print(f'{i} is a valley')
+                valleys.append(i)
+            continue
+    return valleys
 
 def peaks_and_valleys(peaks, valleys):
     print(peaks + valleys)
 
+
+
+def print_exes(data):
+    highest_peak = max(data)
+
+    
+
 def main():
     data = [1, 2, 3, 4, 5, 6, 7, 6, 5, 4, 5, 6, 7, 8, 9, 8, 7, 6, 7, 8, 9]
-    indices = list(range(len(data)))
-
-    peaks_and_valleys(peaks(data, indices), valleys(data, indices))
+    list_of_peaks = []
+    list_of_valleys  = []
+    peaks = peaks(data)
+    valleys = valleys(data)
+    peaks_and_valleys()
     
+
 main()
