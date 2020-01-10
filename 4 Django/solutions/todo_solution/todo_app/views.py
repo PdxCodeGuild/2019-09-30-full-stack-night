54f83d88fba49fa4b2197c1811256eed150befd2
justin <jhiester@gmail.com>
2020-01-10 12:45:33 -0800

    added comments to views.py file

--- 4 Django/solutions/todo_solution/todo_app/views.py
+++ 4 Django/solutions/todo_solution/todo_app/views.py
@@ -1,22 +1,27 @@
 from django.shortcuts import render, redirect
 from django.http import HttpResponse
 
+# import the Todo model from the models file
 from .models import Todo
 
+# view to get all of the todos from the database and send them to the 'todos/list.html' view
 def todo_list(request):
-    todos = Todo.objects.all()
+    todos = Todo.objects.all()  # get all of the todos from the database and store them in todos
     context = {
         'todos': todos
     }
 
     return render(request, 'todos/list.html', context)
 
+
+# view to get retrieve a specific todo from the database and send it to the 'todos/detail.html' view
 def details(request, id):
     todo = Todo.objects.get(id = id)
 
     return render(request, 'todos/detail.html', {"todo": todo})
 
 
+# view add a todo to the database. this view handles both GET and POST HTTP requests
 def add_todo(request):
     if request.method == 'GET':
         return render(request, 'todos/add.html')
@@ -31,18 +36,18 @@ def add_todo(request):
         return redirect('list')
 
 
+# view to remove a specific todo from the database specified by its id
 def remove_todo(request, id):
     todo = Todo.objects.get(id = id)
     todo.delete()
     return redirect('list')
 
-
+# helper function for updating a todo. gets the todo info specified by the id and redirects to its detail page
 def update_todo(request, id):
     todo = Todo.objects.get(id = id)
-    print(request.POST['description'])
     return redirect('details', todo.id)
 
-
+# view to update a todo in the databse specified by its id
 def update(request, id):
     todo = Todo.objects.get(id = id)
     if request.method == 'GET':
@@ -57,6 +62,8 @@ def update(request, id):
         todo.save()
         return redirect('details', todo.id)
 
+
+# mark a todo as done
 def mark_done(request, id):
     todo = Todo.objects.get(id = id)
     todo.status = True
